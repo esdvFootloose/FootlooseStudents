b26d7b5452b8adbe9bdab2daa18cfddb7b10f738
vahe <vahe.abrahamian@pidz.nl>
2022-02-05 14:31:09 +0100

    Add additional logging

--- login/views.py
+++ login/views.py
@@ -8,31 +8,36 @@ from django.conf import settings
 from FootlooseStudents.secret import STUDENT_LOGIN_DISABLED
 
 def login(request):
-    if request.user.is_authenticated:
-        return HttpResponseRedirect('/')
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password'], otp=form.cleaned_data['otp'])
-            if user is not None:
-                if user.is_active:
-                    auth_login(request, user)
-                    if 'next' in request.GET:
-                        if is_safe_url(request.GET['next'], settings.SAFE_URL):
-                            return HttpResponseRedirect(request.GET['next'])
-                    return HttpResponseRedirect('/')
-                else:
-                    return render(request, "base.html", {"message" : "Login failed, your account is deactivated. Please contact the IT committee"})
+    try:
+        if request.user.is_authenticated:
+                return HttpResponseRedirect('/')
+            if request.method == 'POST':
+                form = LoginForm(request.POST)
+                if form.is_valid():
+                    user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password'], otp=form.cleaned_data['otp'])
+                    logger.debug(user)
+                    if user is not None:
+                        if user.is_active:
+                            auth_login(request, user)
+                            if 'next' in request.GET:
+                                if is_safe_url(request.GET['next'], settings.SAFE_URL):
+                                    return HttpResponseRedirect(request.GET['next'])
+                            return HttpResponseRedirect('/')
+                        else:
+                            return render(request, "base.html", {"message" : "Login failed, your account is deactivated. Please contact the IT committee"})
+                    else:
+                        return render(request, "base.html", {
+                            "message": "Login failed, incorrect password. Please <a href='" + reverse(
+                                "login:login") + "'>try again</a>"})
             else:
-                return render(request, "base.html", {
-                    "message": "Login failed, incorrect password. Please <a href='" + reverse(
-                        "login:login") + "'>try again</a>"})
-    else:
-        form = LoginForm()
-    return render(request, 'login.html', {
-        'form': form,
-        'student_login_disabled' : STUDENT_LOGIN_DISABLED
-    })
+                form = LoginForm()
+            return render(request, 'login.html', {
+                'form': form,
+                'student_login_disabled' : STUDENT_LOGIN_DISABLED
+            })
+    except NameError:
+        logger.debug(NameError)
+
 
 def logout(request):
     if not request.user.is_authenticated:
